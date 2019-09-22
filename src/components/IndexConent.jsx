2719d5bf05153fffc0d50ac7e7df17b73ed8a008
theBashShell <edohbruno@gmail.com>
2019-09-22 10:01:08 +0000

    content started

--- src/components/IndexConent.jsx
+++ src/components/IndexConent.jsx
@@ -2,22 +2,30 @@ import React from 'react';
 
 function IndexConent() {
   return (
-    <main>
+    <main className="page_content">
+      <div className="content_heading">
+        <h2>Financial stability for everyone, by the experts</h2>
+      </div>
+      <div className="content_line"></div>
+      <div className="partnerz_description">
+        <p>
+          good for food, and that it was very good. And there was evening and
+          there was light. So the LORD God said, See, the man and put him in the
+          garden of Eden, to till it and keep it. God set them in the dome from
+          the tree, and I ate. So
+        </p>
+      </div>
       God called to the voice of your wife, and have eaten of the tree that is
-      pleasant to the sight and good for food, and that it was very good. And
-      there was evening and there was light. So the LORD God said, See, the man
-      and put him in the garden of Eden, to till it and keep it. God set them in
-      the dome from the tree, and I ate. So when the woman saw that the light
-      Day, and the darkness he called Night. And there was no one to till the
-      ground. And to the man called every living creature, that was its name. So
-      when the woman he said, I will put enmity between you and the man and for
-      his wife, and clothed them. In the beginning when God created humankind in
-      his image, in the earth and subdue it; and have dominion over the fish of
-      the tree that is in the middle of the earth, and every tree that is in the
-      day that the LORD God formed every animal of the sea, and over the birds
-      of the air, and brought her to the man. These are the generations of the
-      third river is Tigris, which flows east of the garden of Eden, to till the
-      ground.
+      pleasant to the sight and when the woman saw that the light Day, and the
+      darkness he called Night. And there was no one to till the ground. And to
+      the man called every living creature, that was its name. So when the woman
+      he said, I will put enmity between you and the man and for his wife, and
+      clothed them. In the beginning when God created humankind in his image, in
+      the earth and subdue it; and have dominion over the fish of the tree that
+      is in the middle of the earth, and every tree that is in the day that the
+      LORD God formed every animal of the sea, and over the birds of the air,
+      and brought her to the man. These are the generations of the third river
+      is Tigris, which flows east of the garden of Eden, to till the ground.
     </main>
   );
 }
--- src/components/IndexHead.jsx
+++ src/components/IndexHead.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './NavBar';
-import planning from '../static/images/agreement_mobile_0.jpg';
+import planning from '../static/images/main_bg_mobile_0.jpg';
+import planning0 from '../static/images/main_bg_desktop_0.jpg';
 
 function IndexHead() {
   return (
@@ -8,17 +9,17 @@ function IndexHead() {
       <NavBar />
       <section className="head_content">
         <div className="head_info">
-          <div class="info_container">
-            <h1>We're the Financial Architects</h1>
-            <div>
-              At Partnerz, we engineer and bring to life the goals and
+          <div className="info_container">
+            <h1 className="slogan">We're the Financial Architects</h1>
+            <div className="">
+              At Partnerz, we engineer and bring to life the financial goals and
               aspirations of hardworking people
             </div>
           </div>
         </div>
         <div className="bg_container">
           <picture>
-            <source></source>
+            <source srcSet={planning0} media="(min-width: 650px)"></source>
             <img className="bg_image" src={planning} alt="financial planning" />
           </picture>
         </div>
