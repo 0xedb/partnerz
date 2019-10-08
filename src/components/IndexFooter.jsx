02aa065d500d8f627ae8ce5fe98cb03f42e8aaf0
theBashShell <edohbruno@gmail.com>
2019-10-08 23:08:07 +0000

    tired

--- src/components/IndexFooter.jsx
+++ src/components/IndexFooter.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import mousekeyboard from '../static/images/keyboardandmouse.svg';
 
 function IndexFooter() {
   return (
     <footer className="page_footer">
-      
+      <small className="copyright">&copy; 2019 Partnerz FP Limited</small>
+      <small className="dev_info">
+        Made with a <img className="mousekeyboard" src={mousekeyboard} alt="mouse and keyboard" /> by
+        <a href="https://github.com/theBashShell" target="_blank">
+          {' '}
+          theBashShell
+        </a>
+      </small>
     </footer>
   );
 }
